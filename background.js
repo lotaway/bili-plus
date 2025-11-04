9f921adad953bce947c3e0f29ca607d1f593eda8
lotaway <576696294@qq.com>
2025-11-04 22:50:24 +0800

    Add video page inject script to get main context(content is islanded).

--- background.js
+++ background.js
@@ -25,12 +25,12 @@ class DownloadManager {
         switch (request.action) {
             case "fetchSubtitles":
             default:
-                this.fetchSubtitles(request.msg, request.options)
+                this.fetchSubtitles(request.msg, sendResponse)
                 break
         }
     }
 
-    async fetchSubtitles(msg) {
+    async fetchSubtitles(msg, sendResponse) {
         const { aid, cid } = msg
         const cookieStore = await chrome.cookies.getAll({
             domain: ".bilibili.com",
@@ -48,13 +48,70 @@ class DownloadManager {
         if (!pref) return sendResponse({ error: "无可用字幕" })
         const subUrl = "https:" + pref.subtitle_url
         const subJson = await fetch(subUrl, { headers }).then((r) => r.json())
-        const downloadId = await chrome.downloads.download({
-            url: imageUrl,
-            filename: filename,
+        let downloadId = -1
+        switch (mode) {
+            case "text":
+                downloadId = this.downloadFile(this.bilisub2text(subJson), `${match.bvid}.md`)
+                break
+            case "srt":
+            default:
+                downloadId = this.downloadFile(this.bilisub2srt(subJson), `${match.bvid}.srt`)
+                break
+        }
+        return sendResponse({ downloadId })
+    }
+
+    float2hhmmss(num) {
+        const int_ = Number.parseInt(num)
+        const frac = Number.parseInt((num - int_) * 1000)
+        const hr = Math.floor(int_ / 3600)
+        const min = Math.floor((int_ % 3600) / 60)
+        const sec = int_ % 60
+        return `${hr}:${String(min).padStart(2, "0")}:${String(sec).padStart(
+            2,
+            "0"
+        )},${String(frac).padStart(3, "0")}`
+    }
+
+    bilisub2text(j) {
+        return j.body.map((s) => s.content).join("\n\n")
+    }
+
+    bilisub2srt(j) {
+        return j.body
+            .map(
+                (s, i) =>
+                    `${i + 1}\n${this.float2hhmmss(
+                        s.from
+                    )} --> ${this.float2hhmmss(s.to)}\n${s.content}`
+            )
+            .join("\n\n")
+    }
+
+    async downloadSrt(srt, name, ext = "srt") {
+        const blob = new Blob([srt], { type: "text/plain" })
+        const url = URL.createObjectURL(blob)
+        const a = document.createElement("a")
+        a.href = url
+        a.download = `${name}.${ext}`
+        a.style.display = "none"
+        document.documentElement.appendChild(a)
+        requestAnimationFrame(() => {
+            a.click()
+            setTimeout(() => {
+                a.remove()
+                URL.revokeObjectURL(url)
+            }, 3000)
+        })
+    }
+
+    async downloadFile(url, filename) {
+        return await chrome.downloads.download({
+            url: url,
+            filename,
             conflictAction: "uniquify",
             saveAs: false,
         })
-        sendResponse({ subJson })
     }
 }
 
--- popup.js
+++ popup.js
@@ -1,36 +1,82 @@
-document.getElementById("extract").onclick = async () => {
-    const [tab] = await init()
-    await extract(tab, "srt")
-    after()
+class PopupController {
+    #videoInfo = {
+        isInit: false,
+        aid: null,
+        cid: null,
+    }
+
+    constructor() {
+        this.initEventHandle()
+        this.initButton()
+    }
+
+    initEventHandle() {
+        document.addEventListener("RequestVideoInfo", async (event) => {
+            const [tab] = await chrome.tabs.query({
+                active: true,
+                currentWindow: true,
+            })
+            await chrome.scripting.executeScript({
+                target: {
+                    tabId: tab.id,
+                },
+                files: ["page_inject.js"],
+                world: "MAIN",
+            })
+        })
+        document.addEventListener("VideoInfoUpdate", (event) => {
+            const { aid, cid } = event.detail
+            this.#videoInfo = { isInit: true, aid, cid }
+        })
+    }
+
+    initButton() {
+        document
+            .getElementById("extract")
+            .addEventListener("click", async () => {
+                await this.extract("srt")
+            })
+
+        document
+            .getElementById("extract-only-text")
+            .addEventListener("click", async () => {
+                await this.extract("text")
+            })
+    }
+
+    async extract(mode = "srt") {
+        document.getElementById("msg").textContent = "正在提取字幕..."
+        let aid = this.#videoInfo.aid
+        let cid = this.#videoInfo.cid
+        if (!cid || !aid) {
+            if (this.#videoInfo.isInit) {
+                alert("视频信息获取失败，请刷新页面重试")
+            } else {
+                alert("content.js maybe not trigger")
+            }
+        }
+        chrome.runtime.sendMessage(
+            {
+                type: "fetchSubtitles",
+                msg: {
+                    aid,
+                    cid,
+                    mode,
+                },
+            },
+            (res) => {
+                if (res?.error) {
+                    alert(res.error)
+                    return
+                }
+                this.afterDownload(res?.downloadId)
+            }
+        )
+    }
+
+    async afterDownload(id) {
+        document.getElementById("msg").textContent = `字幕提取完成:${id}`
+    }
 }
 
-document.getElementById("extract-only-text").onclick = async () => {
-    const [tab] = await init()
-    await extract(tab, "text")
-    after()
-}
-
-async function init() {
-    document.getElementById("msg").textContent = "正在提取字幕..."
-    return await chrome.tabs.query({ active: true, currentWindow: true })
-}
-
-async function extract(tab, mode = "srt") {
-    await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: (args) => {
-            globalThis.__EXT_ARG = args
-        },
-        args: [{ mode }],
-        world: "MAIN",
-    })
-    return await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["content.js"],
-        world: "MAIN",
-    })
-}
-
-async function after() {
-    document.getElementById("msg").textContent = "字幕提取完成"
-}
+new PopupController()
